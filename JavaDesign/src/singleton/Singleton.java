dd50bf0fd5e630b5d34fede84804f5dbbe2e7ae0
jiangzi <205140040@qq.com>
2015-08-09 20:52:42 +0800

    singleton

---
+++ JavaDesign/src/singleton/Singleton.java
@@ -0,0 +1,13 @@
+package singleton;
+
+public class Singleton {
+	public static Singleton singleton=new Singleton();
+	
+	private Singleton(){
+		
+	}
+	
+	public static Singleton getInstance(){
+		return singleton;
+	}
+}
---
+++ JavaDesign/src/singleton/Test.java
@@ -0,0 +1,11 @@
+package singleton;
+
+public class Test {
+	public static void main(String[] args) {
+		Singleton s1=Singleton.getInstance();
+		Singleton s2=Singleton.getInstance();
+		if(s1==s2){
+			System.out.println(s1+"和："+s2+" 是同一个对象");
+		}
+	}
+}
