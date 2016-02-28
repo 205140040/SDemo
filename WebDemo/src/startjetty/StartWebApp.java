c96b7034632ec35debc62fe85a6f5b365640e57a
205140040@qq.com <205140040@qq.com>
2016-02-28 23:19:14 +0800

    jetty

---
+++ WebDemo/src/startjetty/StartWebApp.java
@@ -0,0 +1,22 @@
+package startjetty;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class StartWebApp {
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	public static void main(String[] args) throws Exception {
+		try {
+			@SuppressWarnings("unused")
+			ApplicationContext applicationContext = new FileSystemXmlApplicationContext(
+					new String[] { "D://JAVASoft//GitProject//GitRep//WebDemo//WebContent//WEB-INF//applicationContext.xml" });
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
