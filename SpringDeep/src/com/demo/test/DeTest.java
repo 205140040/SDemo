54a7827294c532a7918f4f9dad13bb6cd17860b7
205140040@qq.com <205140040@qq.com>
2016-03-09 22:14:20 +0800

    8888

---
+++ SpringDeep/src/com/demo/test/DeTest.java
@@ -0,0 +1,13 @@
+package com.demo.test;
+
+public class DeTest {
+	/**
+	 * @author 20514
+	 * 2016Äê3ÔÂ9ÈÕ
+	 * @description 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
