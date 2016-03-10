60a3fd25fbc09de150a68beffdd39f97d6e2c561
205140040@qq.com <205140040@qq.com>
2016-03-10 23:54:21 +0800

    cccccc

--- SpringDeep/src/com/demo/controller/TestController.java
+++ SpringDeep/src/com/demo/controller/TestController.java
@@ -1,8 +1,22 @@
 package com.demo.controller;
 
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/test")
 public class TestController {
-
+	@RequestMapping("/sayHello")
+	public void sayHello(HttpServletResponse response) throws Exception{
+		PrintWriter out=response.getWriter();
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		out.println("hello spring");
+		System.out.println("hello spring");
+		out.close();
+	}
 }
