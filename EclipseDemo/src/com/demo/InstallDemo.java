d204c52edb849ae9a936148ce70d1d8c2d5a51a9
205140040 <205140040@qq.com>
2016-04-24 00:14:55 +0800

    6

---
+++ EclipseDemo/src/com/demo/InstallDemo.java
@@ -0,0 +1,15 @@
+package com.demo;
+/**
+ * 安装插件
+ * 1.help-install-new software
+ * 1.1在work with 输入连接，安装插件
+ * 1.2或者点击add，选择本地下载的插件安装
+ * 2.help-eclipse marplet
+ * @author Administrator
+ * 2016年4月23日
+ * @description 
+ * @ClassName InstallDemo
+ */
+public class InstallDemo {
+
+}
---
+++ EclipseDemo/src/com/demo/ix/ExportImportDemo.java
@@ -0,0 +1,20 @@
+package com.demo.ix;
+
+/**
+ * 导入导出
+ * 
+ * @author Administrator 2016年4月23日
+ * @description
+ * @ClassName ExportImportDemo
+ */
+public class ExportImportDemo {
+
+	public static void main(String[] args) {
+		/**
+		 * 右键导入，
+		 * 右键导出jar文件
+		 * 右键导出
+		 */
+	}
+
+}
