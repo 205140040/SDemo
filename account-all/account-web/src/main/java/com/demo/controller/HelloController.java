cc2dbda820b0abbcfad0aef9ec21425d66577810
205140040 <205140040@qq.com>
2016-05-05 23:12:55 +0800

    caocaocaoccccccccccccccccccc

--- account-all/account-web/src/main/java/com/demo/controller/HelloController.java
+++ account-all/account-web/src/main/java/com/demo/controller/HelloController.java
@@ -1,11 +1,10 @@
 package com.demo.controller;
 
-import com.demo.service.IHelloService;
-
 public class HelloController {
-	private IHelloService helloService;
+	// private IHelloService helloService;
 
 	public void testHello() {
-		helloService.sayHello();
+		// helloService.sayHello();
+		System.out.println("hello");
 	}
 }
