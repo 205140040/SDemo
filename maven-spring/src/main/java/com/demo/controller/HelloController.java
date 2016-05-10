031ad904191cfd67efa29a92b4cf659e930ecd10
205140040 <205140040@qq.com>
2016-05-10 23:11:40 +0800

    lululull

--- maven-spring/src/main/java/com/demo/controller/HelloController.java
+++ maven-spring/src/main/java/com/demo/controller/HelloController.java
@@ -4,18 +4,22 @@ import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
+	
+	private static Logger logger=Logger.getLogger(HelloController.class);
 
 	@RequestMapping("/sayHello")
 	public void sayHello(HttpServletResponse response) throws Exception {
 		response.setContentType("text/html");
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();
+		logger.error("进入了sayHello-----------");
 		out.println("sayHello");
 		out.close();
 	}
